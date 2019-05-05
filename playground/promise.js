63776d13d42b538c448a09f50ef6ed1dfaefca53
leo <leonelvinasco@gmail.com>
2019-05-05 18:12:29 -0500

    advace promises, sum, with two .then ejeuted

--- playground/promise.js
+++ playground/promise.js
@@ -13,20 +13,21 @@ return new Promise((resolve,reject)=>{
 
 asyncAdd(5,7).then((res)=>{
   console.log('result ',res);
+  return asyncAdd(res,33);
 }, (errorMessage)=>{
   console.log(errorMessage);
-});
-
-var somePromise = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
-      resolve('hello promise'); //resolve is used when query go well
-      //reject('Unable to fulfill promise'); //When query went bad
-  },2500);
-
-});
-
-somePromise.then((message)=>{ //if the promise was rejected
-  console.log('Success: ', message);
-}, (errorMessage)=>{
-  console.log('Error: ', errorMessage);
-});
+}).then((res)=>{console.log('should be 45: ',res);},(errorMessage)=>{console.log(errorMessage)});
+//
+// var somePromise = new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//       resolve('hello promise'); //resolve is used when query go well
+//       //reject('Unable to fulfill promise'); //When query went bad
+//   },2500);
+//
+// });
+//
+// somePromise.then((message)=>{ //if the promise was rejected
+//   console.log('Success: ', message);
+// }, (errorMessage)=>{
+//   console.log('Error: ', errorMessage);
+// });
