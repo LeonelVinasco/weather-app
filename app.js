6568b09fe395379e7fc0eb1d3780ceb548a0fc69
leo <leonelvinasco@gmail.com>
2019-05-05 03:37:20 -0500

    llamado al API de darksky

--- app.js
+++ app.js
@@ -1,35 +1,16 @@
-// const fs = require('fs');
-// const _ = require('lodash');
-// const yargs =require('yargs');
-// const request = require('request');
-// const myGeoAPI =require('./leo-maps/myGeoAPI.js');
-// const geoCode= require('./geocode/geocode.js');
-//
-// const argv = yargs
-// .options({
-//    a:{
-//      demand:true,
-//      alias:'address',
-//      describe: 'address to fetch weather for',
-//      string: true //to make sure user add data
-//    }
-// })
-// .help()
-// .alias('help','h')
-// .argv;
-//1f7c04cd3b6d02d7bc49904ebffb58ad
-const request =require('request');
+  const fs = require('fs');
+  const _ = require('lodash');
+  const yargs =require('yargs');
+  const request = require('request');
+  const myGeoAPI =require('./leo-maps/myGeoAPI.js');
+  const geoCode= require('./geocode/geocode.js');
+  const weather=require('./weather/weather.js')
 
-request({
-ulr: 'https://api.darksky.net/forecast/1f7c04cd3b6d02d7bc49904ebffb58ad/37.8267,-122.4233',
-json:true
 
-},(error,response,body)=>{
-  if (error){
-    console.log('Unable to connect to darksky')
-  }else if(response.statusCode === 400){
-    console.log('Unable to fetch weather');
-  }else if(response.statusCode===200){
-    console.log('body.currently.temperature');
+weather.getWeather(37.8267,-122.4233, (ErrorMessage,weatherResults)=>{
+  if(ErrorMessage){
+    console.log(ErrorMessage);
+  }else{
+    console.log(JSON.stringify(results,undefined,2);
   }
-})
+});
---
+++ weather/weather.js
@@ -0,0 +1,22 @@
+const request =require('request');
+
+var getWeather= (lat,long, callback)=>{
+  request({
+  url: `https://api.darksky.net/forecast/1f7c04cd3b6d02d7bc49904ebffb58ad/${lat},${long}`,
+  json:true
+
+  },(error,response,body)=>{
+    if (error){
+      callback('Unable to connect to darksky1');
+    }else if(response.statusCode === 400){
+      callback('Unable to fetch weather');
+    }else if(response.statusCode===200){
+      callback(undefined,body.currently.temperature);
+    }
+  });
+
+}
+
+module.exports={
+  getWeather
+}
