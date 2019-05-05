484845252d0251702b42114351722656839d02c0
leo <leonelvinasco@gmail.com>
2019-05-04 22:04:28 -0500

    Adding search words to the URI

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ const yargs =require('yargs');
 const request = require('request');
 const myGeoAPI =require('./leo-maps/myGeoAPI.js');
 
-request({
-  url:'https://api.nasa.gov/planetary/apod?api_key=',
-  //json:true,
-  eventUri:'http://eventregistry.org/event/eng-4766398',
-  resultType: 'articleUris',
-  keyword:'trump',
+const argv = yargs
+.options({
+   a:{
+     demand:true,
+     alias:'address',
+     describe: 'address to fetch weather for',
+     string: true //to make sure user add data
+   }
+})
+.help()
+.alias('help','h')
+.argv;
 
+var encodedAddress=encodeURIComponent(argv.address);
+
+
+console.log(`argv+${argv.address}`);
+request({
+  url:`https://api.nasa.gov/planetary/apod?api_key=vgeo26FRotjabELs3fhIxCQ7W1iH4CeMrwAAbzmN?city=${encodedAddress}`,
+  json:true,
 },(error,response,body)=>{
-  console.log(body);
+  console.log(body.date);
+  console.log(body.url);
+  console.log(body.title);
 });
